c2a9dc751268d8f6918c1e808e5541b85db5824c
dapodev <apolonik.danila2002@gmail.com>
2022-06-06 12:01:25 +0300

    feat: implemented validation for search filter

--- src/tasks/dto/getTasksFilter.dto.ts
+++ src/tasks/dto/getTasksFilter.dto.ts
@@ -1,7 +1,15 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 import { ETaskStatus } from '../task.model';
 
 class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(ETaskStatus)
   status?: ETaskStatus;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   search?: string;
 }
 
