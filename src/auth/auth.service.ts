69546f445b6957642aeb0193faf401322ae21a50
dapodev <apolonik.danila2002@gmail.com>
2022-06-07 17:56:47 +0300

    implemented unique username feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -18,6 +22,14 @@ export class AuthService {
 
     const user = await this.usersRepository.create({ username, password });
 
-    await this.usersRepository.save(user);
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException(`Username '${username}' already exists`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 import { TaskStatus } from './taskStatus.model';
 
-@Entity()
+@Entity('tasks')
 export class Task {
   @PrimaryGeneratedColumn('uuid')
   id: string;
